fef6492a04fe2712cdf7f82cded26d8581d2557c
disna-sistemas <sistemas@disna.com>
2017-10-23 12:52:05 +0200

    .

--- dsn_product_logistics/models/mrp.py
+++ dsn_product_logistics/models/mrp.py
@@ -27,15 +27,15 @@ class MrpProduction(models.Model):
 
         for record in self:
             break
-            packaging_ids = record.product_tmpl_id.packaging_ids.filtered(lambda x: x.dsn_default == True).sorted(key=lambda x: x.id, reverse=False)
-#            if packaging_ids:
-#                record.dsn_packaging_id = packaging_ids[0]
+            packaging_ids = record.product_tmpl_id.packagings.filtered(lambda x: x.dsn_default == True).sorted(key=lambda x: x.id, reverse=False)
+            if packaging_ids:
+                record.dsn_packaging_id = packaging_ids[0]
 
     dsn_packaging_id = fields.Many2one(comodel_name='product.packaging',
                                     string='Packaging',
-                                    domain="[('id','in', product_tmpl_id.packaging_ids)]",
-#                                   default='_compute_default_packaging',
-                                   store=True)
+                                    domain="[('id','in', product_tmpl_id.packagings)]",
+                                    default='_compute_default_packaging',
+                                    store=True)
 
 #    @api.multi0
 #    @api.depends('product_id')
--- sincron.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#rsync -arv --rsh=/usr/bin/ssh /opt/odoo/v8/disna-sistemas odoo@192.168.5.21:/opt/odoo/v8
-sshpass -p bat1bi2 rsync -arv /opt/odoo/v8/disna-sistemas odoo@192.168.5.21:/opt/odoo/v8
