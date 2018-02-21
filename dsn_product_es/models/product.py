b47877006488437fb30a6e5a7176589631563dc5
disna-sistemas <sistemas@disna.com>
2018-02-21 16:38:05 +0100

    Abort product_template es and en translations

--- dsn_product_es/models/product.py
+++ dsn_product_es/models/product.py
@@ -22,24 +22,22 @@ import logging
 class ProductTemplateTranslation(models.Model):
     _inherit = "ir.translation"
 
-    @api.multi
-    @api.onchange('src','value')
-    def onchange_lang_es_ES(self):
-        _logger=logging.getLogger(__name__)
-        prod_obj = self.env['product.template']
-#        for record in self:
-#            if record.name=='product_template,name' and record.lang=='es_ES':
-
-        for record in self.filtered(lambda x: x.type=='model' and x.name=='product.template,name' and x.lang=='es_ES'):
-            _logger.info('IR TRANSLATION ' + record.name + ' ' + str(record.res_id))
-            prod_ids = prod_obj.search([('id','=',str(record.res_id))])
-            if prod_ids:
-                prod = prod[0]
-                _logger.info('PRODUCT' + prod.default_code)
-                prod.write({'dsn_name_es': record.value,
-                            'dsn_name_en': record.src})
-#                prod.dsn_name_es = record.value
-#                prod.dsn_name_en = record.src
+#     @api.multi
+#     @api.onchange('src','value')
+#     def onchange_lang_es_ES(self):
+#         _logger=logging.getLogger(__name__)
+#         prod_obj = self.env['product.template']
+#
+#         for record in self.filtered(lambda x: x.type=='model' and x.name=='product.template,name' and x.lang=='es_ES'):
+#             _logger.info('IR TRANSLATION ' + record.name + ' ' + str(record.res_id))
+#             prod_ids = prod_obj.search([('id','=',str(record.res_id))])
+#             if prod_ids:
+#                 prod = prod[0]
+#                 _logger.info('PRODUCT' + prod.default_code)
+#                 prod.write({'dsn_name_es': record.value,
+#                             'dsn_name_en': record.src})
+# #                prod.dsn_name_es = record.value
+# #                prod.dsn_name_en = record.src
 
 class product(models.Model):
 
