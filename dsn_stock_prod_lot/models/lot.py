cf83bc3241dc6234cb061d09fec510173870f7c5
disna-sistemas <sistemas@disna.com>
2018-06-19 13:01:25 +0200

    .

--- dsn_stock_prod_lot/models/lot.py
+++ dsn_stock_prod_lot/models/lot.py
@@ -238,8 +238,8 @@ class dsnStockProductionLot(models.Model):
                                                         'product_id': prod.id,
                                                         'version_id': None if ver is None else ver.id})
                         lot_comps.append(lot_comp)
-                    for comp in values['dsn_component_ids']:
-                        values['dsn_component_ids'] = [(2, comp.id)]
+
+                    values['dsn_component_ids'] = [(2, comp.id) for comp in values['dsn_component_ids']]
                     values['dsn_component_ids'] = [(6,0, [x.id for x in lot_comps])]
 
                 # if comps:
