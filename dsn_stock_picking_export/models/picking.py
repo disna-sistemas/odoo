75d7400fa5d41452f85a27c2121e706b2fe2bcc7
disna-sistemas <sistemas@disna.com>
2018-06-27 14:19:09 +0200

    .

--- dsn_stock_picking_export/models/picking.py
+++ dsn_stock_picking_export/models/picking.py
@@ -101,14 +101,15 @@ class dsnStockPickingExport(models.Model):
                 if line.product_id.dsnidart:
                     _dsnidart = self.replace_bars(str(line.product_id.dsnidart))
 
-                for quant in line.reserved_quant_ids.mapped('lot_id'):
-                    lot = etree.SubElement(docdata, "lot",
+#                for quant in line.reserved_quant_ids.mapped('lot_id'):
+                for lot in line.reserved_quant_ids.mapped('lot_id'):
+                    lotdata = etree.SubElement(docdata, "lot",
                            {
                                "product_id": str(line.product_id.id), etree.QName(xsi, "type"): etree.QName(xsd, "string"),
                                "product_code": line.product_id.default_code, etree.QName(xsi, "type"): etree.QName(xsd, "string"),
                                "product_dsnidart": _dsnidart, etree.QName(xsi, "type"): etree.QName(xsd, "string"),
                                "product_name": line.product_id.product_tmpl_id.dsn_name_es, etree.QName(xsi, "type"): etree.QName(xsd, "string"),
-                               "lot_name": quant.lot_id.name, etree.QName(xsi, "type"): etree.QName(xsd, "string"),
+                               "lot_name": lot.name, etree.QName(xsi, "type"): etree.QName(xsd, "string"),
 #                               "lot_qty": str(sum(quant.qty)), etree.QName(xsi, "type"): etree.QName(xsd, "string")
                                "lot_qty": str(sum(line.reserved_quant_ids.filtered(lambda x: x.lot_id == lot).mapped('qty')))
                            })
