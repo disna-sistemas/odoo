9dee5467545f3848d3ccaf1acaf62d038e0f94c9
disna-sistemas <sistemas@disna.com>
2021-05-31 15:50:43 +0200

    button paises

--- dsn_company/models/company.py
+++ dsn_company/models/company.py
@@ -22,11 +22,9 @@ import logging
 class dsnCompany(models.Model):
     _inherit = "res.company"
 
-    _logger = logging.getLogger(__name__)
-
     @api.multi
     def dsn_button_do_work(self):
-        lista = ['4749S','4750S','4747S','4748S']
+        lista = ['4749S','4750S','4747S','4748S','4743S','4744S','4745S','4746S','4677S','4678S','4757S','4758S','4755S','4756S','4751S','4752S','4753S','4754S','4749EP','4750EP','4747EP','4748EP','4743EP','4744EP','4745EP','4746EP','4677EP','4678EP','4757EP','4758EP','4755EP','4756EP','4751EP','4752EP','4753EP','4754EP']
 
         prodobj = self.env['product.product']
         for cod in lista:
@@ -38,15 +36,12 @@ class dsnCompany(models.Model):
                     prodv = prodvs[0]
                     ctrylist = []
                     for pavc in prod.country_list:
-                        ident = prodv.country_list.create({'product_id': prodv.product_id, 'country_id': pavc.country_id,
-                                                           'cc': pavc.cc, 'date_to': pavc.date_to, 'date_from': pavc.date_from}).id
+                        ident = prodv.country_list.create({'product_id': prodv.product_id, 'country_id': pavc.country_id.id, 'cc': pavc.cc, 'date_to': pavc.date_to, 'date_from': pavc.date_$
                         ctrylist.append(ident)
-#                    print lista
-                    prodv.country_list = [6, 0, ctrylist]
-
-                    _logger.info('ASIGNADOs países de ' + prodv)
+                    prodv.country_list = [(6, 0, ctrylist)]
 
         return True
+
 
     dsn_logo2 = fields.Binary('Logo Instituto Naturvita', help='Logo for certificates')
     dsn_logo_export = fields.Binary('Export Logo for documents', help='Logo for export docs')
