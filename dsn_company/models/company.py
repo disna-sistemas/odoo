851999e4e594f7a5c9861f1502e98442e7963f50
disna-sistemas <sistemas@disna.com>
2021-05-31 16:23:34 +0200

    button paises

--- dsn_company/models/company.py
+++ dsn_company/models/company.py
@@ -24,8 +24,8 @@ class dsnCompany(models.Model):
 
     @api.multi
     def dsn_button_do_work(self):
-        lista = ['4749S','4750S','4747S','4748S','4743S','4744S','4745S','4746S','4677S','4678S','4757S','4758S','4755S','4756S','4751S','4752S','4753S','4754S','4749EP','4750EP','4747EP','4748EP','4743EP','4744EP','4745EP','4746EP','4677EP','4678EP','4757EP','4758EP','4755EP','4756EP','4751EP','4752EP','4753EP','4754EP']
-
+#        lista = ['4749S','4750S','4747S','4748S','4743S','4744S','4745S','4746S','4677S','4678S','4757S','4758S','4755S','4756S','4751S','4752S','4753S','4754S','4749EP','4750EP','4747EP','4748EP','4743EP','4744EP','4745EP','4746EP','4677EP','4678EP','4757EP','4758EP','4755EP','4756EP','4751EP','4752EP','4753EP','4754EP']
+        lista = ['5033S','5034S','5035S','5036S','5045S','5046S','5037S','5038S','2945S','2946S','2951S','2952S','2949S','2950S','2947S','2948S','4180S','4181S']
         prodobj = self.env['product.product']
         for cod in lista:
             prods = prodobj.search([('default_code','=',cod)])
@@ -36,7 +36,7 @@ class dsnCompany(models.Model):
                     prodv = prodvs[0]
                     ctrylist = []
                     for pavc in prod.country_list:
-                        ident = prodv.country_list.create({'product_id': prodv.product_id, 'country_id': pavc.country_id, 'cc': pavc.cc,
+                        ident = prodv.country_list.create({'product_id': prodv.product_id, 'country_id': pavc.country_id.id, 'cc': pavc.cc,
                                                            'date_to': pavc.date_to, 'date_from': pavc.date_from, 'notes': pavc.notes })
                         ctrylist.append(ident)
                     prodv.country_list = [(6, 0, ctrylist)]
